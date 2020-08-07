28c1f0651390480b54baef531dd8e107ec03f5c7
Mateusz Armatys <mateusz.armatys@gmail.com>
2020-08-07 15:41:35 +0200

    Set `App` as application in AndroidManifest; install Timber for debug builds

--- app/src/main/java/pl/makenika/todos/App.java
+++ app/src/main/java/pl/makenika/todos/App.java
@@ -3,11 +3,15 @@ package pl.makenika.todos;
 import android.app.Application;
 
 import dagger.hilt.android.HiltAndroidApp;
+import timber.log.Timber;
 
 @HiltAndroidApp
 public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        }
     }
 }
